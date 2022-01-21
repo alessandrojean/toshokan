112b6b55ecc2d74645d8ee8e12afa4e5c5786d21
Alessandro Jean <alessandrojean@gmail.com>
2022-01-21 12:24:39 -0300

    Update the icons using IconKitchen.

--- vite.config.js
+++ vite.config.js
@@ -24,23 +24,23 @@ export default defineConfig({
         start_url: '/dashboard',
         icons: [
           {
-            src: './img/icons/android-chrome-192x192.png',
+            src: './img/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: './img/icons/android-chrome-512x512.png',
+            src: './img/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: './img/icons/android-chrome-maskable-192x192.png',
+            src: './img/icons/icon-192x192-maskable.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable'
           },
           {
-            src: './img/icons/android-chrome-maskable-512x512.png',
+            src: './img/icons/icon-512x512-maskable.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
